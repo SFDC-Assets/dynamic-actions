a05297b5ad653c6041d65596b895e59f29d51750
Stefan Zepeda <omarat@me.com>
2024-11-04 17:56:07 +0100

    Update orgInit.sh - added new set password line
    Added set password line to be compliant with other demos

--- orgInit.sh
+++ orgInit.sh
@@ -1,5 +1,6 @@
 sf demoutil org create scratch -f config/project-scratch-def.json -d 3 -s -p customer -e dynactions.demo
 sf project deploy start
+sf demoutil user password set -p salesforce1 -g User -l User
 sf org assign permset -n AppBuilder
 sf automig load -d data --concise
 sf org open -p /lightning/o/Vehicle__c/list?filterName=Recent
