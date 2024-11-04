b45ea815ec2ee78dc392b3584a82ac8e9800ff44
Stefan Zepeda <omarat@me.com>
2024-11-04 17:51:52 +0100

    Update orgInit.sh to new SF commands
    Updated all commands to SF CLI equivalents

--- orgInit.sh
+++ orgInit.sh
@@ -1,5 +1,5 @@
-sfdx force:org:create -f config/project-scratch-def.json -d 3 -s
-sfdx force:source:push
-sfdx force:user:permset:assign -n AppBuilder
-sfdx automig:load -d data --concise
-sfdx force:org:open -p /lightning/o/Vehicle__c/list?filterName=Recent
+sf demoutil org create scratch -f config/project-scratch-def.json -d 3 -s -p customer -e dynactions.demo
+sf project deploy start
+sf org assign permset -n AppBuilder
+sf automig load -d data --concise
+sf org open -p /lightning/o/Vehicle__c/list?filterName=Recent
